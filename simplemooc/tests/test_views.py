5d749affb367392ad47c52446d2042a7c9252435
juliotoscano <jjortb@cin.ufpe.br>
2016-07-28 14:33:24 -0300

    Teste com falha

--- simplemooc/tests/test_views.py
+++ simplemooc/tests/test_views.py
@@ -6,37 +6,21 @@ class courseCreate(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.url = reverse('createCourse')
+        self.url = reverse('simplemooc:createCourse')
 
     def test_course_ok(self):
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'courseForm.html')
-        data = {
-            'name':'Django para Zumbis',
-            'slug':'DZ',
-            'description':'Este curso e para zumbis',
-            'qtd_available':'30',
-            'qtd_students':'20',
-            'teacher':'gileno',
-            'category':'python'
-            }
-        response = self.client.post(self.url, data)
-        self.assertEquals(response.status_code, 200)
-        self.assertTrue('success' in response.context)
+       response = self.client.get(self.url)
+       self.assertEquals(response.status_code, 200)
+       self.assertTemplateUsed(response, 'courseForm.html')
+       data = {'name': 'Django para Zumbis', 'slug': 'DZ', 'description': 'Este curso e para zumbis', 'program': 'Esta e a programacao', 'qtd_available': '30', 'qtd_students': '20', 'begin_matric': '', 'end_matric': '', 'begin_course': '', 'end_course': '', 'image': '', 'is_approved': '', 'teacher': 'Gileno', 'category': 'Python', 'modules': 'Desenvolvendo uma aplicacao Web', 'keywords': ''}
+       response = self.client.post(self.url, data)
+       self.assertEquals(response.status_code, 200)
+       self.assertTrue('success' in response.context)
 
     def test_course_error(self):
-        data ={
-            'name':'Django para Zumbis',
-            'slug':'DZ',
-            'description':'Este curso e para zumbis',
-            'qtd_available':'30',
-            'qtd_students':'20',
-            'teacher':'gileno',
-        }
+        data = {'name': 'Django para Zumbis', 'slug': 'DZ', 'description': 'Este curso e para zumbis', 'qtd_available': '30', 'qtd_students': '20', 'teacher': 'Gileno', 'category': 'Python', 'modules': 'Desenvolvendo uma aplicacao Web'}
         response = self.client.post(self.url, data)
         self.assertEquals(response.status_code, 200)
         self.assertTrue('success' not in response.context)
-
 
 
